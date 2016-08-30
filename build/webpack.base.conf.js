5a367d5a63c3fb0d47987dc49747f636aca025ee
景天 <jingtian@naitang.com>
2016-08-30 16:24:45 +0800

    autoprefixer

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,6 +2,7 @@
 
 var path = require('path')
 var webpack = require('webpack')
+var autoprefixer = require('autoprefixer')
 var config = require('../config')
 var utils = require('./utils')
 var projectRoot = path.resolve(__dirname, '../')

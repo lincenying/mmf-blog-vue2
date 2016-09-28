26d05b75a8dbe9ed340ff97c5c3e8d83f9b52594
景天 <jingtian@naitang.com>
2016-09-28 12:37:26 +0800

    *

--- src/api/index.js
+++ src/api/index.js
@@ -25,7 +25,7 @@ export default {
             $.ajax({ data: config }).then(data => {
                 resolve(data)
             }, error => {
-                store.dispatch('showMsg', error.responseText.toString())
+                store.dispatch('showMsg', error.responseText || error.statusText)
                 reject(error)
             })
         })
@@ -35,7 +35,7 @@ export default {
             $.ajax({ global: false, data: config }).then(data => {
                 resolve(data)
             }, error => {
-                store.dispatch('showMsg', error.responseText.toString())
+                store.dispatch('showMsg', error.responseText || error.statusText)
                 reject(error)
             })
         })

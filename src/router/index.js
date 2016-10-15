cfc7dc04405542fc16fa37076eceddb8d9a2a9b0
lincenying <lincenying@qq.com>
2016-10-15 14:38:27 +0800

    *

--- src/router/index.js
+++ src/router/index.js
@@ -38,12 +38,12 @@ const router = new VueRouter({
     scrollBehavior,
     routes: [
         { name:'index', path: '/', component: index },
-        { name:'index', path: '/category/:id(\\d+)', component: index },
-        { name:'index', path: '/search/:qs', component: index },
+        { name:'category', path: '/category/:id(\\d+)', component: index },
+        { name:'search', path: '/search/:qs', component: index },
         { name:'article', path: '/article/:id', component: article, meta: { scrollToTop: true } },
-        { path: '/list/:page(\\d+)', component: adminList, meta: { scrollToTop: true }, beforeEnter: guardRoute },
-        { path: '/post', component: adminPost, meta: { scrollToTop: true }, beforeEnter: guardRoute },
-        { path: '/edit/:id/:page', component: adminEdit, meta: { scrollToTop: true }, beforeEnter: guardRoute }
+        { name:'list', path: '/list/:page(\\d+)', component: adminList, meta: { scrollToTop: true }, beforeEnter: guardRoute },
+        { name:'post', path: '/post', component: adminPost, meta: { scrollToTop: true }, beforeEnter: guardRoute },
+        { name:'edit', path: '/edit/:id/:page', component: adminEdit, meta: { scrollToTop: true }, beforeEnter: guardRoute }
     ]
 })
 

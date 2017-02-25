4a26667778833775beaea40028a43a8ab5e9f880
lincenying <lincenying@qq.com>
2017-02-25 17:04:34 +0800

    no message

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,29 +1,38 @@
-/* global require, module, process */
+/* global require, module, process, path */
 
 var path = require('path')
 var express = require('express')
+var webpack = require('webpack')
 var config = require('../config')
-
 var proxyMiddleware = require('http-proxy-middleware')
+var webpackConfig = require('./webpack.dev.config')
 
 // default port where dev server listens for incoming traffic
 var port = process.env.PORT || config.dev.port
-// Define HTTP proxies to your custom API backend
-// https://github.com/chimurai/http-proxy-middleware
+    // Define HTTP proxies to your custom API backend
+    // https://github.com/chimurai/http-proxy-middleware
 var proxyTable = config.dev.proxyTable
 
 var app = express()
+var compiler = webpack(webpackConfig)
 
-var indexHTML
-var adminHTML
+var devMiddleware = require('webpack-dev-middleware')(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    stats: {
+        colors: true,
+        chunks: false
+    }
+})
 
-require('./setup-dev-server')(app, {
-    indexUpdated: index => {
-        indexHTML = index
-    },
-    adminUpdated: index => {
-        adminHTML = index
-    },
+var hotMiddleware = require('webpack-hot-middleware')(compiler)
+    // force page reload when html-webpack-plugin template changes
+compiler.plugin('compilation', function(compilation) {
+    compilation.plugin('html-webpack-plugin-after-emit', function(data, cb) {
+        hotMiddleware.publish({
+            action: 'reload'
+        })
+        cb()
+    })
 })
 
 // proxy api requests
@@ -36,35 +45,27 @@ Object.keys(proxyTable).forEach(function(context) {
     }
     app.use(proxyMiddleware(context, options))
 })
+
+// handle fallback for HTML5 history API
+var history = require('connect-history-api-fallback')
+app.use(history({
+    rewrites: [
+        { from: 'index', to: '/index.html'},
+        { from: /\/backend/, to: '/admin.html'},
+        { from: /^\/backend\/.*$/, to: '/admin.html'},
+    ]
+}))
+
+// serve webpack bundle output
+app.use(devMiddleware)
+
+// enable hot-reload and state-preserving
+// compilation error display
+app.use(hotMiddleware)
+
 // serve pure static assets
-var staticPath = path.posix.join(config.build.assetsPublicPath, config.build.assetsSubDirectory)
+var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
-
-// 前台路由
-app.get(['/', '/category/:id', '/search/:key', '/article/:id', '/about', '/trending/:by', '/user/account', '/user/password'], (req, res) => {
-    res.send(indexHTML)
-})
-
-// 后台渲染
-app.get(['/backend', '/backend/*'], (req, res) => {
-    res.send(adminHTML)
-})
-
-// 404 页面
-app.get('*', (req, res) => {
-    res.send('HTTP STATUS: 404')
-})
-
-app.use(function(req, res, next) {
-    var err = new Error('Not Found')
-    err.status = 404
-    next(err)
-})
-
-app.use(function(err, req, res) {
-    res.status(err.status || 500)
-    res.send(err.message)
-})
 
 module.exports = app.listen(port, function(err) {
     if (err) {

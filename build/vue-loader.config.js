69ae17f78c2647bd2b32fd7634692fe1d42668ea
lincenying <lincenying@qq.com>
2017-05-16 17:01:51 +0800

    web pack config

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -3,15 +3,15 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var loaders = {}
 if (process.env.NODE_ENV === 'production') {
     loaders = {
-        css: 'vue-style-loader!css-loader!postcss-loader',
-        less: 'vue-style-loader!css-loader!postcss-loader!less-loader'
+        css: 'vue-style-loader!css-loader',
+        less: 'vue-style-loader!css-loader!less-loader'
     }
 } else {
     loaders = {
-        css: ExtractTextPlugin.extract({fallback: 'vue-style-loader', use: 'css-loader!postcss-loader'}),
-        less: ExtractTextPlugin.extract({fallback: 'vue-style-loader', use: 'css-loader!postcss-loader!less-loader'})
+        css: ExtractTextPlugin.extract({fallback: 'vue-style-loader', use: 'css-loader'}),
+        less: ExtractTextPlugin.extract({fallback: 'vue-style-loader', use: 'css-loader!less-loader'})
     }
 }
 module.exports = {
-    loaders: loaders
+    loaders
 }

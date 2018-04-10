4b24f7f3202512af8b61a3a8dad96829fb272c2b
林岑影 <lincenying@qq.com>
2018-04-11 00:15:32 +0800

    fix extract-text-webpack-plugin

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,5 +1,5 @@
 /* global require, module, process */
-const path = require("path")
+const path = require('path')
 const config = require('../config')
 const webpack = require('webpack')
 const merge = require('webpack-merge')
@@ -7,19 +7,24 @@ const baseWebpackConfig = require('./webpack.base.config')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 config.build.productionSourceMap = false
 
 module.exports = merge(baseWebpackConfig, {
     mode: 'production',
     module: {
-        rules: [{
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract(['css-loader', 'postcss-loader'])
-        },  {
-            test: /\.less/,
-            loader: ExtractTextPlugin.extract(['css-loader', 'postcss-loader', 'less-loader'])
-        }]
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.less/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'less-loader'],
+            },
+        ],
     },
     devtool: config.build.productionSourceMap ? '#source-map' : false,
     output: {
@@ -30,79 +35,82 @@ module.exports = merge(baseWebpackConfig, {
     },
     optimization: {
         runtimeChunk: {
-            name: "manifest"
+            name: 'manifest',
         },
         splitChunks: {
             cacheGroups: {
                 vendor: {
                     test: /[\\/]node_modules[\\/]/,
-                    name: "vendors",
+                    name: 'vendors',
                     priority: -20,
-                    chunks: "all"
-                }
-            }
+                    chunks: 'all',
+                },
+            },
         },
         minimizer: [
             new UglifyJsPlugin({
                 uglifyOptions: {
                     compress: {
-                        warnings: false
-                    }
+                        warnings: false,
+                    },
                 },
                 sourceMap: config.build.productionSourceMap,
-                parallel: true
-            })
-        ]
+                parallel: true,
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ],
     },
     plugins: [
         new webpack.DefinePlugin({
             'process.env': {
-                NODE_ENV: '"production"'
-            }
+                NODE_ENV: '"production"',
+            },
         }),
         // new webpack.optimize.OccurenceOrderPlugin(),
         // extract css into its own file
-        new ExtractTextPlugin('static/css/[name].[contenthash:7].css'),
+        // new ExtractTextPlugin('static/css/[name].[contenthash:7].css'),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: 'static/css/[name].[contenthash:7].css',
+            chunkFilename: 'static/css/[id].[contenthash:7].css',
+        }),
         // generate dist index.html with correct asset hash for caching.
         // you can customize output by editing /index.html
         // see https://github.com/ampedandwired/html-webpack-plugin
         new HtmlWebpackPlugin({
-            chunks: [
-                'manifest', 'vendors', 'app'
-            ],
+            chunks: ['manifest', 'vendors', 'app'],
             filename: 'index.html',
             template: 'src/template/index.html',
             inject: true,
             minify: {
                 removeComments: true,
                 collapseWhitespace: true,
-                removeAttributeQuotes: true
+                removeAttributeQuotes: true,
             },
-            chunksSortMode (chunk1, chunk2) {
-                const orders = ['manifest', 'vendors', 'app'];
-                const order1 = orders.indexOf(chunk1.names[0]);
-                const order2 = orders.indexOf(chunk2.names[0]);
+            chunksSortMode(chunk1, chunk2) {
+                const orders = ['manifest', 'vendors', 'app']
+                const order1 = orders.indexOf(chunk1.names[0])
+                const order2 = orders.indexOf(chunk2.names[0])
                 return order1 - order2
-            }
+            },
         }),
         new HtmlWebpackPlugin({
-            chunks: [
-                'manifest', 'vendors', 'admin'
-            ],
+            chunks: ['manifest', 'vendors', 'admin'],
             filename: 'admin.html',
             template: 'src/template/admin.html',
             inject: true,
             minify: {
                 removeComments: true,
                 collapseWhitespace: true,
-                removeAttributeQuotes: true
+                removeAttributeQuotes: true,
             },
-            chunksSortMode (chunk1, chunk2) {
-                const orders = ['manifest', 'vendors', 'admin'];
-                const order1 = orders.indexOf(chunk1.names[0]);
-                const order2 = orders.indexOf(chunk2.names[0]);
+            chunksSortMode(chunk1, chunk2) {
+                const orders = ['manifest', 'vendors', 'admin']
+                const order1 = orders.indexOf(chunk1.names[0])
+                const order2 = orders.indexOf(chunk2.names[0])
                 return order1 - order2
-            }
+            },
         }),
-    ]
+    ],
 })
